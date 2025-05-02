d9337dc0e2fceb455557d33dca34f22a14e530b5
Bartosz Bogdanowicz <wurir1994@gmail.com>
2025-05-02 16:29:14 +0200

    'add' logic

--- js/app.js
+++ js/app.js
@@ -13,9 +13,16 @@ Calculator.prototype.getHistoryAsString = function() {
 
 Calculator.prototype.add = function(num1, num2) {
     // 1. zamień wartości przekazane przez parametr na typ number
+    const num1Number = Number(num1)
+    const num2Number = Number(num2)
+    let result 
     // 2. sprawdź czy są one poprawne
-    // 3. jeśli tak to wykonaj działanie i zapisz jego resultat
-    // 4. dodaj do historii operacji to działanie w fomie: 1 + 1 = 2
+    if(typeof num1Number === 'number' && typeof num2Number === 'number') {
+        // 3. jeśli tak to wykonaj działanie i zapisz jego rezultat
+        result = num1Number + num2Number
+    }
+    // 4. dodaj do historii operacji to działanie w formie: 1 + 1 = 2
+    this.history.push(num1 + ' + ' + num2 + ' = ' + result)
 }
 
 const calc = new Calculator();
